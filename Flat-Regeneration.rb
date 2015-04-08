e26637991b3a58ea90c2cfd1c0b325ee56894923
Feldherren <rpaliwoda@googlemail.com>
2015-04-08 18:33:40 +0100

    Now supports enemy notebox

--- Flat-Regeneration.rb
+++ Flat-Regeneration.rb
@@ -1,16 +1,22 @@
 =begin
-Flat Regeneration Rates v0.1, by Feldherren (rpaliwoda@googlemail.com)
+Flat Regeneration Rates v0.2, by Feldherren (rpaliwoda@googlemail.com)
 
 Updates:
-  v0.1 - flat regeneration enabled for HP, SP and TP
+  v0.1 - flat HP, SP and TP regeneration enabled for actors
+  v0.2 - enemies, too
 
-VX Ace loves percentages. I don't. This script adds non-scaled flat regeneration rates for HP, MP and TP. This doesn't overwrite any percentage-based regeneration battlers have going on; it just allows for a character to regenerate 1 MP a turn no matter what happens, if you want.
+VX Ace loves percentages. I don't. This script adds non-scaled flat regeneration
+rates for HP, MP and TP. This doesn't overwrite any percentage-based 
+regeneration battlers have going on; it just allows for a character to 
+regenerate 1 MP (or 5, or whatever) a turn no matter what happens, if you want.
 
-Actor tags:
+Actor/Enemy tags:
   <regenerate hp: x>
   <regenerate mp: x>
   <regenerate tp: x>
-    Put any of these tags in an actor's notebox. They'll regenerate x hp/mp/tp as specified at the end of the turn, in addition to any percentage-based rates they have through default VX Ace.
+    Put any of these tags in an actor's notebox. They'll regenerate x hp/mp/tp 
+    as specified at the end of the turn, in addition to any percentage-based 
+    rates they have through default VX Ace.
 =end
 module Regeneration
   MATCH_HP = /<regenerate hp:\s*(\d*)>/i
@@ -26,6 +32,8 @@ class Game_Battler < Game_BattlerBase
     :regenerate_hp_edit
     if self.actor?
       self.hp += ($data_actors[self.id].note.match(Regeneration::MATCH_HP) ? $data_actors[self.id].note.match(Regeneration::MATCH_HP)[1].to_i : 0)
+    else
+      self.hp += ($data_enemies[self.enemy_id].note.match(Regeneration::MATCH_HP) ? $data_enemies[self.enemy_id].note.match(Regeneration::MATCH_HP)[1].to_i : 0)
     end
   end
   #--------------------------------------------------------------------------
@@ -36,6 +44,8 @@ class Game_Battler < Game_BattlerBase
     :regenerate_mp_edit
     if self.actor?
       self.mp += ($data_actors[self.id].note.match(Regeneration::MATCH_MP) ? $data_actors[self.id].note.match(Regeneration::MATCH_MP)[1].to_i : 0)
+    else
+      self.mp += ($data_enemies[self.enemy_id].note.match(Regeneration::MATCH_MP) ? $data_enemies[self.enemy_id].note.match(Regeneration::MATCH_MP)[1].to_i : 0)
     end
   end
   #--------------------------------------------------------------------------
@@ -46,6 +56,8 @@ class Game_Battler < Game_BattlerBase
     :regenerate_tp_edit
     if self.actor?
       self.tp += ($data_actors[self.id].note.match(Regeneration::MATCH_TP) ? $data_actors[self.id].note.match(Regeneration::MATCH_TP)[1].to_i : 0)
+    else
+      self.tp += ($data_enemies[self.enemy_id].note.match(Regeneration::MATCH_TP) ? $data_enemies[self.enemy_id].note.match(Regeneration::MATCH_TP)[1].to_i : 0)
     end
   end
 end
